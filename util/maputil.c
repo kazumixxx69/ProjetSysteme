a5f01c4ebbd566e86ddec7bf31411c842a1cad3e
Salem33 <33433553+Salem33@users.noreply.github.com>
2017-11-23 17:33:12 +0100

    V3.j'ai_oublié setheight marche

--- util/maputil.c
+++ util/maputil.c
@@ -233,7 +233,7 @@ int set_height(char* filename, int value){
    status = read(fd, &old_value, sizeof(int));
    status = lseek(fd, (off_t) (-1) * sizeof(int), SEEK_CUR);
    status = write(fd, &buf, sizeof(int));
-   status = lseek(fd, 1 * sizeof(int), SEEK_SET);
+   //status = lseek(fd, 1 * sizeof(int), SEEK_SET);
    status = read(fd, &nb_objects, sizeof(int));
    for (int i = 0; i < nb_objects; i++){
       int obj_name_length;
@@ -245,26 +245,31 @@ int set_height(char* filename, int value){
    fprintf(stderr, "old val %d\n", old_value);
    fprintf(stderr, "val %d\n", value);
 
-   lseek(fd, 1 * sizeof(int), SEEK_CUR);
+   //lseek(fd, 1 * sizeof(int), SEEK_CUR);
 
    while((status = read(fd, &buf, sizeof(int))) > 0){
 	fprintf(stderr, "x: %d\n", buf);
-	lseek(fd, 2 * sizeof(int), SEEK_CUR);         
-	status = lseek(fd, (off_t) -3 * sizeof(int), SEEK_CUR);
-	off_t cur_position = (off_t) status;
-        fprintf(stderr, "curpos: %d\n", (int) cur_position);
-        status = lseek(fd, 0, SEEK_END);
-	off_t file_end = (off_t) status;
-	fprintf(stderr, "filend: %d\n", (int) file_end);
-        char buffer[file_end - cur_position - 3 * sizeof(int)];
-	fprintf(stderr, "sizeofint: %d\n", (int) sizeof(char));
-	lseek(fd, cur_position + 3 * sizeof(int), SEEK_SET);
-	read(fd, &buffer, (file_end - cur_position - 3 * sizeof(int)));
-	lseek(fd, cur_position, SEEK_SET);
-	int newHeight = buf-(old_value-value);
-	write(fd, &newHeight, (file_end - cur_position -3*sizeof(int)));
-	ftruncate(fd, file_end - 3 * sizeof(int));
-        lseek(fd, cur_position, SEEK_SET);
+        read(fd, &buf, sizeof(int));
+        int new_height = buf-(old_value-value);
+        lseek(fd, (off_t) -1 * sizeof(int), SEEK_CUR);
+	write(fd, &new_height, sizeof(int));
+	lseek(fd, sizeof(int), SEEK_CUR);
+        if(new_height < 0){
+            status = lseek(fd, (off_t) -3 * sizeof(int), SEEK_CUR);
+            off_t cur_position = (off_t) status;
+            fprintf(stderr, "curpos: %d\n", (int) cur_position);
+            status = lseek(fd, 0, SEEK_END);
+            off_t file_end = (off_t) status;
+	    fprintf(stderr, "filend: %d\n", (int) file_end);
+            char buffer[file_end - cur_position - 3 * sizeof(int)];
+	    fprintf(stderr, "sizeofint: %d\n", (int) sizeof(char));
+            lseek(fd, cur_position + 3 * sizeof(int), SEEK_SET);
+            status = read(fd, &buffer, (file_end - cur_position - 3 * sizeof(int)));
+            lseek(fd, cur_position, SEEK_SET);
+            write(fd, &buffer, (file_end - cur_position - 3 * sizeof(int)));
+            ftruncate(fd, file_end - 3 * sizeof(int));
+            lseek(fd, cur_position, SEEK_SET);
+        }
    }
    if (status < 0){
       close(fd);
